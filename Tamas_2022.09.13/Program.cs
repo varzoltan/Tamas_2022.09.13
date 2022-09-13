6819d01fcaf7382b2250401433e39006c7957830
Zoltán Varga <varzoltan@tanarium.hu>
2022-09-13 15:15:10 +0200

    Add project files.

---
+++ Tamas_2022.09.13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//.NET kollekciók halmaza
+
+namespace Tamas_2022._09._13//névtér: alapesetben tartalmazza a project nevét.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Egysoros megjegyzés
+
+            //Többsoros megjegyzés
+            /*
+             * 
+             * 
+             * Készítette: Varga Zoltán
+             * 
+             * 
+             * 
+             */
+            //Egyszerű típusok
+            int szam = 15;//Egész szám
+            int szam3 = 30;
+            float szam1 = 14.21f;//Egyszeres lebegőpontos szám (Valós szám)
+            double szam2 = 123.12;//Dupla lebegőpontos, valós szam.
+            int osszeg = szam + szam3;
+            int szorzata = szam * szam3;
+            int kulonbseg = szam * szam3;
+            double hanyadosa = szam / (double)szam3;//Típuskényszerítés, cascadolás
+            double hanyadosa2 = szam / 14.0;   
+
+            Console.WriteLine("A két szám hányadosa: " + hanyadosa);
+            Console.WriteLine("A két szám hányadosa: " + hanyadosa2);
+            
+            //Karaktertípus
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tamas_2022.09.13/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tamas_2022.09.13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tamas_2022.09.13")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f67a2a53-1e01-4bdd-b510-1743b6d0532a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
