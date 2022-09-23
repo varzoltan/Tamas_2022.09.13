82ee56fcc1a16b963dabfd2907e2883cbc1a92b9
Zoltán Varga <varzoltan@tanarium.hu>
2022-09-23 15:19:13 +0200

    2022.09.23., 15:19

--- Tamas_2022.09.13/Program.cs
+++ Tamas_2022.09.13/Program.cs
@@ -7,9 +7,9 @@ using System.IO;//.NET kollekciók halmaza
 
 namespace Tamas_2022._09._13//névtér: alapesetben tartalmazza a project nevét.
 {
-    internal class Program
+    internal class Program//osztály
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//Főprogram: csak az fut le, amit ide írok!
         {
             //Egysoros megjegyzés
 
@@ -29,16 +29,32 @@ namespace Tamas_2022._09._13//névtér: alapesetben tartalmazza a project nevét
             double szam2 = 123.12;//Dupla lebegőpontos, valós szam.
             int osszeg = szam + szam3;
             int szorzata = szam * szam3;
-            int kulonbseg = szam * szam3;
+            int kulonbseg = szam - szam3;
             double hanyadosa = szam / (double)szam3;//Típuskényszerítés, cascadolás
-            double hanyadosa2 = szam / 14.0;   
+            double hanyadosa2 = szam / 14.0;//Az egész számot valós értékre cserélem
+            float lebegopontos = 14.2f;
 
             Console.WriteLine("A két szám hányadosa: " + hanyadosa);
             Console.WriteLine("A két szám hányadosa: " + hanyadosa2);
-            
-            //Karaktertípus
 
-            Console.ReadKey();
+            //Karaktertípus
+            char karakter = 'a';
+
+            //Szöveges típus
+            string szam5 = "12345";
+            string szoveg = "alma";
+            string szoveg2 = "fa";
+            string osszefuz = szoveg + szoveg2;
+            int hossz = szam5.Length;
+            char[] karaktertomb = szam5.ToCharArray();
+            Console.WriteLine(szoveg.Replace('a', 'e')+"\n"+osszefuz + " " + karaktertomb[2]);
+
+            //Logikai
+            //Két értéket vehet fel: True, False
+            bool igaz = true;
+            Console.WriteLine(igaz);
+
+            Console.ReadKey();//Megállítja a program futását. Vár egy billentyű leütésre
         }
     }
 }
